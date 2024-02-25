6b9e911d54f2f0beede55049ce7bfb9457d62006
Anuj Krishna <anuj.krishna.phuyal@gmail.com>
2024-02-25 16:34:13 +0545

    second version changes made

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".text", {
+
+    strings: ["ML Engineer", "Python Developer"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+
+})
+
+document.getElementById("button-38").onclick = function () {
+    window.open("https://www.linkedin.com/in/anuj-krishna-phuyal-0a3487251/", "_blank");
+};
